a687eca8fe11b59993da5c1301ee58d34898ac0a
nikulaj <aJns@users.noreply.github.com>
2015-08-24 16:14:26 +0300

    Add sleeping to gameloop

--- src/GameLoop.cc
+++ src/GameLoop.cc
@@ -10,8 +10,6 @@
 
 void GameEngine::gameLoop(const bool& shutDown, GameEngine::Logic& gameLogic) {
     const std::chrono::duration<double> TIME_PER_UPDATE(0.033);
-
-
     std::chrono::time_point<std::chrono::system_clock> previousTime,
         currentTime;
     std::chrono::duration<double> lag(0.0);
@@ -21,12 +19,12 @@ void GameEngine::gameLoop(const bool& shutDown, GameEngine::Logic& gameLogic) {
         lag += (currentTime - previousTime);
         previousTime = currentTime;
 
-        while(lag >= TIME_PER_UPDATE) {
-
-            std::cout << lag.count() << " : " << TIME_PER_UPDATE.count() << std::endl;
-
+        while (lag >= TIME_PER_UPDATE) {
             gameLogic.updateLogic();
             lag -= TIME_PER_UPDATE;
+
         }
+
+        std::this_thread::sleep_for(TIME_PER_UPDATE - lag);
     }
 }
