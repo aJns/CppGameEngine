6122fd2f520a3873aff150c42f6086bd12910182
nikulaj <aJns@users.noreply.github.com>
2015-08-26 13:21:29 +0300

    Fix python interface
    Gameloop can now execute simple python scripts.

--- src/GameLoop.cc
+++ src/GameLoop.cc
@@ -14,6 +14,7 @@
 void GameEngine::gameLoop(const bool& shutDown, GameEngine::Logic& gameLogic) {
 
     Py_Initialize();
+    std::cout << "Using Python " << Py_GetVersion() << std::endl;
 
     const std::chrono::duration<double> TIME_PER_UPDATE(0.033);
     std::chrono::time_point<std::chrono::system_clock> previousTime,
@@ -37,6 +38,16 @@ void GameEngine::gameLoop(const bool& shutDown, GameEngine::Logic& gameLogic) {
         try {
             GameEngine::visibleMsg("Executing script...");
             /* boost::python::exec_file("test.py"); */
+
+
+            namespace python = boost::python;
+
+            // Create the python environment
+            python::object main = python::import("__main__");
+            python::object global(main.attr("__dict__"));
+
+            /* python::exec("print('hello world')", global, global); */
+            python::object result = python::exec_file("test.py", global, global);
         }
         catch(...) {
             std::cout << "Python error!" << std::endl;
