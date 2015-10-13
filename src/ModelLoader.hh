fa920ca2d86f8cbf7531e1654420b2d14b6f84eb
nikulaj <aJns@users.noreply.github.com>
2015-10-13 15:01:20 +0300

    Add ModelLoader
    Loads 3d models for use with Magnum, using anySceneImporter.

---
+++ src/ModelLoader.hh
@@ -0,0 +1,51 @@
+// Magnum
+#include <Corrade/PluginManager/Manager.h>
+#include <Corrade/Utility/Arguments.h>
+#include <Magnum/Buffer.h>
+#include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/TextureFormat.h>
+#include <Magnum/Mesh.h>
+#include <Magnum/PixelFormat.h>
+#include <Magnum/Renderer.h>
+#include <Magnum/ResourceManager.h>
+#include <Magnum/Texture.h>
+#include <Magnum/MeshTools/Compile.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
+#include <Magnum/SceneGraph/MatrixTransformation3D.h>
+#include <Magnum/SceneGraph/Scene.h>
+#include <Magnum/Shaders/Phong.h>
+#include <Magnum/Trade/AbstractImporter.h>
+#include <Magnum/Trade/ImageData.h>
+#include <Magnum/Trade/MeshData3D.h>
+#include <Magnum/Trade/MeshObjectData3D.h>
+#include <Magnum/Trade/PhongMaterialData.h>
+#include <Magnum/Trade/SceneData.h>
+#include <Magnum/Trade/TextureData.h>
+
+#include "configure.h"
+
+
+namespace GameEngine {
+    typedef Magnum::ResourceManager<Magnum::Buffer, Magnum::Mesh,
+            Magnum::Texture2D, Magnum::Shaders::Phong,
+            Magnum::Trade::PhongMaterialData> ViewerResourceManager;
+    typedef
+        Magnum::SceneGraph::Object<Magnum::SceneGraph::MatrixTransformation3D>
+        Object3D;
+    typedef
+        Magnum::SceneGraph::Scene<Magnum::SceneGraph::MatrixTransformation3D>
+        Scene3D;
+
+
+    class ModelLoader {
+    public:
+        explicit ModelLoader();
+    private:
+        void addObject(Magnum::Trade::AbstractImporter& importer, Object3D*
+                parent, Magnum::UnsignedInt i);
+        ViewerResourceManager _resourceManager;
+        Magnum::SceneGraph::DrawableGroup3D _drawables;
+    };
+}
